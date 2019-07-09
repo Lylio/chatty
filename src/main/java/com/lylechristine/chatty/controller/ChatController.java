7c39d33a133e0d74a2b6828f8abb0624b6b436f4
Lyle Christine <lyle.christine@cybg.com>
2019-07-09 10:38:37 +0100

    update

--- src/main/java/com/lylechristine/chatty/controller/ChatController.java
+++ src/main/java/com/lylechristine/chatty/controller/ChatController.java
@@ -21,6 +21,7 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage,
                                SimpMessageHeaderAccessor headerAccessor) {
+
         // Add username in web socket session
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;

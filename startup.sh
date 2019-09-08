ae4345294e0ac7fc4dc9f67ed175237871d058e3
Lyle Christine <cxz@gmx.com>
2019-09-08 15:09:50 +0100

    startup.sh update

--- startup.sh
+++ startup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 start_spring_boot() {
-  echo "**Spring Boot running**"
+  echo "**Spring Boot start-up script running**"
+  USER root
+  chmod 775 ./mvnw
   ./mvnw spring-boot:run
 }
 

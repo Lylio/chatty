bedc821a9c051edbb56b64a62039b8c8915affd3
lylio <cxz@gmx.com>
2019-10-10 21:43:11 +0100

    fake unit tesy

---
+++ src/test/java/com/lylechristine/chatty/FakeTests.java
@@ -0,0 +1,19 @@
+package com.lylechristine.chatty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FakeTests {
+
+    @Test
+    public void sum() {
+        assertEquals(5, 4);
+    }
+
+}
+
